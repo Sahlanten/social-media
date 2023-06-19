61b141b7702eec4521e11ba26ea491bd046cf4b0
Sahlanten <kkipcharsky@gmail.com>
2023-06-19 23:34:34 +0200

    lesson 34

--- src/Render.js
+++ src/Render.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import state, { addPost } from './redux/state';
+import { addPost, updateNewPostText } from './redux/state';
 
 
-export let rerenderEntireTree = () => {
+export let rerenderEntireTree = (state) => {
 	const root = ReactDOM.createRoot(document.getElementById('root'));
 	root.render(
-		<App state={state} addPost={addPost} />
+		<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import state from './redux/state';
 import { rerenderEntireTree } from './Render';
 
-rerenderEntireTree();
+rerenderEntireTree(state);
 
 
 
